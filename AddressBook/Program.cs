5acba1ae8c0f65e2fa4d2c6e6994f8780f1aaf59
unknown <abhimvj575@gmail.com>
2022-12-15 14:20:57 +0530

    [Abhilash] Added: Create a Contact in AddressBook

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace AddressBook
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Program");
+            addressbook book = new addressbook();
+            book.CreateContact();
         }
     }
 }
